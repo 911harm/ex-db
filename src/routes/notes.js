44c09872b0253fbf0ed9ebe836ecba03fd52e3b6
911harm <911harm@gmail.com>
2020-09-06 19:34:22 -0500

    creando controladores

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,14 +1,43 @@
 const express= require('express');
 const router= express.Router();
-
+const Note=require('../models/Note')
 
 
 router.get('/notes/new',(req,res)=>{
 res.render('notes/new')
 })
 
-router.get('/notes/',(req,res)=>{
-res.send('notes de la base de datos')
+router.post('/notes/new-note',async (req,res)=>{
+const {title,content}=req.body;
+console.log(title,content);
+let errors=[];
+if(!title){
+    errors.push({text:"Por favor ingrese un Titulo"})
+}
+if(!content){
+    errors.push({text:"Por favor ingrese la nota"})
+}
+if(errors.length>0){
+    console.log(errors);
+    res.render('notes/new',{
+        title,
+        content,
+        errors
+    })
+}
+else{
+    const newNote= new Note({title,content});
+    console.log(newNote);
+    await newNote.save();
+    res.redirect('/notes/');
+    
+}
+})//fin metodo de recibir
+
+router.get('/notes/',async(req,res)=>{
+const notes= await Note.find();
+console.log(notes);
+res.render('notes/all',{notes})
 })
 
 
